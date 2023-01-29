de1a0595f3a40c9b487b4038c61c00eaad81b036
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-29 18:49:30 +0530

    lab cycle-1

--- prime.c
+++ prime.c
@@ -5,9 +5,11 @@ printf("enter the number : ");
 scanf("%d",&num);
 for(int i=2;i<num;i++){
 if(num%i==0){
+printf("Not a prime");
 break;
 }else{
 printf("prime number!!");
+break;
 }
 }
 return 0;
@@ -16,3 +18,6 @@ return 0;
 output :
 enter the number : 5
 prime number!!
+-------------------------------------
+enter the number : 4
+Not a prime
