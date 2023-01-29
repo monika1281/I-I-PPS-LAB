b527768924fca7c9d2e7eafc3277979167925f21
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-29 18:34:41 +0530

    lab cycle-1

---
+++ prime_in_between.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+int n1,n2,count=0;
+printf("enter the numbers :");
+scanf("%d%d",&n1,&n2);
+for(int i=n1+1;i<n2;i++){
+for(int j=2;j<i;j++){
+if(i%j==0){
+count++;
+}
+}
+if(count==0){
+printf("%d\n",i);
+}
+count=0;
+}
+return 0;
+}
+-----------------------
+output :
+enter the numbers :8 20
+11
+13
+17
+19
