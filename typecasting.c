2c177093d5dc8303ea17ec463214c350f8e9e237
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-29 17:00:52 +0530

    lab cycle-1

---
+++ typecasting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int a,b;
+float c,d;
+printf("enter the numbers a and b");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%f\n",c);
+
+printf("enter the number ");
+scanf("%d",&num);
+printf("the character is %c",num);
+
+return 0;
+}
+-----------------------
+output:
