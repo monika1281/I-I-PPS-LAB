6e96d0b0bf1e874f23d444d0f0276d5d2653c479
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-30 20:22:42 +0530

    lab cycle-2

---
+++ fact_with_recursion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+int n;
+printf("Enter a number:");
+scanf("%d",&n);
+int result;
+result=fact(n);
+printf("result:%d",result);
+return 0;
+}
+int fact(int n)
+{
+int result;
+if(n==0)
+{
+result=1;
+}
+else{
+result=n*fact(n-1);
+}
+return result;
+}
+---------------------------------------------
+output:-
+Enter a number: 5
+result:120
