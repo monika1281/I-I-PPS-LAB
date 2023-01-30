63349aec642cd22e8134708b26466e693d47d716
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-30 21:18:58 +0530

    lab cycle-2

---
+++ sum_array_elements_using_pointers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+	int i,n,*ptr;
+	int sum=0;
+	printf("Enter size of the array:");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the values of the array:");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	ptr=&a[0];
+	for(i=0;i<n;i++){
+  sum=sum+*ptr;
+		ptr=ptr+1;
+	}
+	printf("sum of array elements using pointers is %d",sum);
+	return ;
+	
+}
+--------------------------------------------------------------
+output:
+Enter size of the array: 5
+Enter the values of the array: 1 2 3 4 5
+sum of array elements using pointers is 15
