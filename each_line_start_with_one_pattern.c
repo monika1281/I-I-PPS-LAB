bb775b152942527953cf817473409dcdbc63176e
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-01-31 22:14:10 +0530

    lab cycle-3

---
+++ each_line_start_with_one_pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int i, j;
+int rows;
+printf("Enter the number of rows:");
+scanf("%d",&rows);
+for(i=1;i<=rows;i++)
+{
+    for(j=1;j<=I;j++)
+    {
+        printf ("%2d ",j);
+        
+    }
+    printf ("\n");
+}
+
+    return 0;
+}
+Output:-
+Enter the number of rows:3
+1 
+1  2 
+1  2  3
