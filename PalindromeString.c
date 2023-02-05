a5fbdebb519395f8f6d7a9fbf279dbc05b9c38fa
monika1281 <120377661+monika1281@users.noreply.github.com>
2023-02-05 14:20:47 +0530

    Lab Cycle - 3

---
+++ PalindromeString.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char string1[20];
+    int i, length;
+    int flag = 0;
+    
+    printf("Enter a string:");
+    scanf("%s", string1);
+    
+    length = strlen(string1);
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+           }
+        }
+    
+    if (flag==1) {
+        printf("%s is not a palindrome", string1);
+    }    
+    else {
+        printf("%s is a palindrome", string1);
+    }
+    return 0;
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+OUTPUT:
+
+Enter a string: madam
+madam
+madam is a palindrome
+
+OUTPUT-2:
+
+Enter a string: MONIKA
+MONIKA
+MONIKA is not a palindrome
